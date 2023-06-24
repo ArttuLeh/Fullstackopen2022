e178e1c92a2572e4cb830d0d553c9b31fcce321c
ArttuLeh <lehtovaara.arttu@gmail.com>
2023-06-24 16:54:34 +0300

    9.4-9.5 done, codes that are not needed are commented

--- osa9/fist-step/bmiCalculator.ts
+++ osa9/fist-step/bmiCalculator.ts
@@ -1,25 +1,41 @@
-const calculateBmi = (height: number, weight: number) => {
+/*interface MultiplyValues {
+  weight: number;
+  height: number;
+}
+const parseArguments = (args: string[]): MultiplyValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};*/
+
+export const calculateBmi = (height: number, weight: number) => {
   height = height / 100;
   const bmi = weight / (height * height);
-  console.log(bmi);
+  //console.log(`Your bmi is ${bmi}`);
   if (bmi < 19) {
     return 'slightly underweight';
   } else if (bmi >= 19 && bmi < 25) {
     return 'Normal (healty weight)';
-  } else if (bmi >= 25) {
+  } else {
     return 'slightly overweight';
   }
 };
 
-try {
-  console.log(calculateBmi(180, 75));
+/*try {
+  const { height, weight } = parseArguments(process.argv);
+  calculateBmi(height, weight);
 } catch (error: unknown) {
   let errorMessage = 'something went wrong';
   if (error instanceof Error) {
     errorMessage += ' Error ' + error.message;
   }
   console.log(errorMessage);
-}
-//call function console.log(calculateBmi(180, 74))
-
-//print: Normal (healthy weight)
+}*/
